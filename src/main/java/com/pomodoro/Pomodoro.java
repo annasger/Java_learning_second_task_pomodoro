292c8c2e1408e63173e23d8d8ac2b4ebb1868ec8
unknown <annasger>
2022-06-12 17:00:52 +0300

    first commit

---
+++ src/main/java/com/pomodoro/Pomodoro.java
@@ -0,0 +1,10 @@
+package com.pomodoro;
+
+public class Pomodoro {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello pomodoro!");
+        System.out.println("Hello !!!!");
+    }
+}
